68347dd1f2cf2dfbdda4c1eb8c01eae7dd7e4ba0
multiful <129200984+multiful@users.noreply.github.com>
2024-06-07 15:23:15 +0900

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,7 +1,11 @@
 import streamlit as st  # streamlit 라이브러리 임포트
+import pandas as pd  # pandas 라이브러리 임포트
+import numpy as np   # numpy 라이브러리 임포트
+from PIL import Image     # 이미지 처리를 위한 PIL 라이브러리 임포트
+import os  # 파일 경로 확인을 위한 os 라이브러리 임포트
 
 # 타이틀 텍스트 출력
-st.title('첫번째 웹 어플 만들기 👋')
+st.title('첫번째 웹 어플 만들기👋')
 
 # 텍스트 출력
 st.write('# 1. Markdown 텍스트 작성하기')
@@ -22,11 +26,9 @@ st.markdown(
     ### 마크다운 헤더3
     일반 텍스트
     '''
-    )
+)
 
 # DataFrame 출력
-import pandas as pd  # pandas 라이브러리 임포트
-
 st.write('# 2. DataFrame 표시하기')  # 텍스트 출력
 df = pd.DataFrame({  # DataFrame 생성
     '이름': ['홍길동', '이순신', '강감찬'],
@@ -36,16 +38,21 @@ df = pd.DataFrame({  # DataFrame 생성
 st.dataframe(df)  # DataFrame 출력
 
 # 그래프 출력
-import numpy as np   # numpy 라이브러리 임포트
-
 st.write('# 3. 그래프 표시하기')  # 텍스트 출력
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"]) # DataFrame 생성
 
 st.bar_chart(chart_data)  # 바 차트 출력
 
 # 이미지 출력
-from PIL import Image     # 이미지 처리를 위한 PIL 라이브러리 임포트
-
 st.write('# 4. 이미지 표시하기')   # 텍스트 출력
-img = Image.open('chi.jpg')    # 이미지 파일 열기
-st.image(img, width=300)          # 이미지 출력
+
+# 이미지 파일 경로
+image_path = 'C:/StreamlitImages/chi.png'
+
+# 파일 존재 여부 확인
+if os.path.exists(image_path):
+    st.write(f"이미지 파일 경로: {image_path}")
+    img = Image.open(image_path)     # 이미지 파일 열기
+    st.image(img, width=300)         # 이미지 출력
+else:
+    st.write(f"이미지 파일을 찾을 수 없습니다. 경로를 확인해주세요: {image_path}")
