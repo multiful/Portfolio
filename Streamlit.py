0896de72c27dd927dbbd89d88168a36ae6a70cca
multiful <129200984+multiful@users.noreply.github.com>
2024-06-07 14:57:49 +0900

    Update Streamlit.py
    last

--- Streamlit.py
+++ Streamlit.py
@@ -23,3 +23,28 @@ st.markdown(
     일반 텍스트
     '''
     )
+# DataFrame 출력
+import pandas as pd  # pandas 라이브러리 임포트
+
+st.write('# 2. DataFrame 표시하기')  # 텍스트 출력
+df = pd.DataFrame({  # DataFrame 생성
+    '이름': ['홍길동', '이순신', '강감찬'],
+    '나이': [20, 45, 35]
+})
+
+st.dataframe(df)  # DataFrame 출력
+
+# 그래프 출력
+import numpy as np   # numpy 라이브러리 임포트
+
+st.write('# 3. 그래프 표시하기')  # 텍스트 출력
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"]) # DataFrame 생성
+
+st.bar_chart(chart_data)  # 바 차트 출력
+
+# 이미지 출력
+from PIL import Image     # 이미지 처리를 위한 PIL 라이브러리 임포트
+
+st.write('# 4. 이미지 표시하기')   # 텍스트 출력
+img = Image.open('chi.png')    # 이미지 파일 열기
+st.image(img, width=300)          # 이미지 출력
